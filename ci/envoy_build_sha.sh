c6abd27bdcead77b1534277867e050e9c97bdc47
Piotr Sikora <piotrsikora@google.com>
2017-10-17 13:43:32 -0700

    ci: update build image to 1d6f34b8.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- ci/envoy_build_sha.sh
+++ ci/envoy_build_sha.sh
@@ -1,3 +1,3 @@
 # If you edit the SHA here you must also edit the SHA in .circleci/config.yml.
 
-ENVOY_BUILD_SHA=8839608c2f4b0d7c8311361ee5c8785e543d517a
+ENVOY_BUILD_SHA=1d6f34b8d1616fff68d638921f69c66e2ecf8500
