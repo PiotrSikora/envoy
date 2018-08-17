96cd8f2e82f27c106b47b101fcb30639a81ebb69
Piotr Sikora <piotrsikora@google.com>
2018-08-17 01:16:13 +0000

    ci: update clang to version 6.0 in the CentOS build image.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- ci/build_container/build_container_centos.sh
+++ ci/build_container/build_container_centos.sh
@@ -3,14 +3,30 @@
 # scl devtoolset and epel repositories
 yum install -y centos-release-scl epel-release
 
-# llvm-5.0.0 repository from copr
-curl -L -o /etc/yum.repos.d/alonid-llvm-5.0.0-epel-7.repo \
-  https://copr.fedorainfracloud.org/coprs/alonid/llvm-5.0.0/repo/epel-7/alonid-llvm-5.0.0-epel-7.repo
+# llvm-6.0.0 repository from https://llvm.cpp.fail
+cat >/etc/yum.repos.d/llvm.repo <<EOF
+[llvm]
+name=LLVM for CentOS-generic
+baseurl=https://llvm.cpp.fail/CentOS/generic/llvm/x86_64/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://llvm.cpp.fail/gpg.key
+EOF
+
+LLVM_VERSION=6.0.0
+yum install -y clang-${LLVM_VERSION} llvm-${LLVM_VERSION} lld-${LLVM_VERSION} \
+               libc++-${LLVM_VERSION} libc++-devel-${LLVM_VERSION}
+# not LLVM, but needed for builds with clang.
+yum install -y libstdc++-static glibc-static
+
+ln -s /usr/bin/clang++ /usr/bin/clang++-6.0
+ln -s /usr/bin/clang-format /usr/bin/clang-format-6.0
 
 # dependencies for bazel and build_recipes
 yum install -y java-1.8.0-openjdk-devel unzip which openssl rpm-build \
                cmake3 devtoolset-4-gcc-c++ git golang libtool make ninja-build patch rsync wget \
-               clang-5.0.0 devtoolset-4-libatomic-devel llvm-5.0.0 python-virtualenv bc
+               devtoolset-4-libatomic-devel python-virtualenv bc
 yum clean all
 
 ln -s /usr/bin/cmake3 /usr/bin/cmake
@@ -25,20 +41,8 @@ chmod ug+x "./${BAZEL_INSTALLER}"
 "./${BAZEL_INSTALLER}"
 rm "./${BAZEL_INSTALLER}"
 
-# symbolic links for clang
-pushd /opt/llvm-5.0.0/bin
-ln -s clang++ clang++-5.0
-ln -s clang-format clang-format-5.0
-popd
-
-mkdir -p /usr/lib/llvm-5.0/bin
-pushd /usr/lib/llvm-5.0/bin
-ln -s /opt/llvm-5.0.0/bin/llvm-symbolizer .
-popd
-
 # setup bash env
 echo '. scl_source enable devtoolset-4' > /etc/profile.d/devtoolset-4.sh
-echo 'PATH=/opt/llvm-5.0.0/bin:$PATH' > /etc/profile.d/llvm-5.0.0.sh
 
 # enable devtoolset-4 for current shell
 # disable errexit temporarily, otherwise bash will quit during sourcing
@@ -47,3 +51,4 @@ set +e
 set -e
 
 EXPECTED_CXX_VERSION="g++ (GCC) 5.3.1 20160406 (Red Hat 5.3.1-6)" ./build_container_common.sh
+#EXPECTED_CXX_VERSION="g++ (GCC) 7.3.1 20180303 (Red Hat 7.3.1-5)" ./build_container_common.sh
